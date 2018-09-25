5ea5a8ad01db1d26b50c4057b75ff66413d3fffd
Thomas <pajamacruzer@gmail.com>
2018-09-25 09:35:06 -0500

    Turd

---
+++ GroupProject/src/groupProject/GroupProject.java
@@ -0,0 +1,9 @@
+package groupProject;
+
+public class GroupProject {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ GroupProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sawatzkyt
+ *
+ */
+module GroupProject {
+}
